f3ac52b5c5708b8e1747ae50b538fc9ee3abfeba
Ludwig <ludwig.slettingdalen@gmail.com>
2022-08-31 16:32:45 +0200

    Commit

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,24 +1,46 @@
 import { useQuery } from "react-query";
 import Character from "./Character";
+import { useState } from "react";
 
 export const Characters = () => {
 
-    const fetchCharacters = async () => {
-        const response = await fetch('https://rickandmortyapi.com/api/character')
+    const [page, setPage] = useState(1)
+
+    const fetchCharacters = async ({ queryKey }) => {
+        const response = await fetch(`https://rickandmortyapi.com/api/character?page=${queryKey[1]}`)
         return response.json()
     }
 
-    const { data, status } = useQuery('characters', fetchCharacters)
+    const { data, status } = useQuery(['characters', page], fetchCharacters, {
+        keepPreviousData: true
+    })
 
     if (status === 'loading') { return <div>Loading...</div> }
     if (status === 'error') { return <div>Error</div> }
 
     return (
-        <div className="flex flex-wrap justify-center bg-slate-900">
-            {data.results.map((character) => (
-                <Character key={character.id} character={character} />
-            ))}
-        </div>
+        <>
+            <div className="flex flex-wrap justify-center bg-slate-900">
+                {data.results.map((character) => (
+                    <Character key={character.id} character={character} />
+                ))}
+            </div>
+            <div className="flex justify-center bg-slate-900 gap-2">
+                <button
+                    className="bg-gray-300 rounded-sm p-1 w-20"
+                    onClick={() => { setPage((previous) => previous - 1) }}
+                    disabled={page === 1}>
+                    Previous
+                </button>
+                <button
+                    className="bg-gray-300 rounded-sm p-1 w-20"
+                    onClick={() => { setPage((previous) => previous + 1) }}
+                    disabled={!data.info.next}
+                >
+                    Next
+                </button>
+            </div>
+        </>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <header className=' bg-slate-900 text-4xl text-center text-white text-bold p-5'>Rick Ann Morty</header>
+        <header className=' bg-slate-900 text-4xl text-center text-white text-bold p-5'>Rick And Morty</header>
     )
 }
 
